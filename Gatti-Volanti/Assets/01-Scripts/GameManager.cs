d0a6790d7385a5021fb1916ee3160a3365305cff
Alexjkz <aespomobile@gmail.com>
2023-12-18 18:28:06 +0100

    Sistema Procedurale non funziona granchè

--- Gatti-Volanti/Assets/01-Scripts/GameManager.cs
+++ Gatti-Volanti/Assets/01-Scripts/GameManager.cs
@@ -8,18 +8,27 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject prefabStage;
     private Coroutine MyCoroutineInstance;
     [SerializeField] private bool isReached = false;
+    private float spawnPoint;
+    private float stageLength;
     
     // Start is called before the first frame update
     void Start()
     {
+        stageLength = prefabStage.GetComponent<Renderer>().bounds.size.x;
+
+        MyCoroutineInstance = StartCoroutine(GenerazioneProceduraleMondo());
+
+        spawnPoint = playerGatto.transform.position.x;
+
         MyCoroutineInstance = StartCoroutine(GenerazioneProceduraleMondo());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerGatto.transform.position.x > 20)
+        if(playerGatto.transform.position.x >= spawnPoint - stageLength)
         {
+            spawnPoint += stageLength;
             MyCoroutineInstance = StartCoroutine(GenerazioneProceduraleMondo());
         }
         print(playerGatto.transform.position.x);
@@ -27,8 +36,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator GenerazioneProceduraleMondo()
     {
-        GameObject newObject = Instantiate(prefabStage, new Vector3(20, 0, 0.5f), Quaternion.identity);
-        
+        GameObject newObject = Instantiate(prefabStage, new Vector3(spawnPoint + stageLength/2 , 0, 0.5f), Quaternion.identity);
         yield return new WaitUntil(() => isReached);
     }
 }
